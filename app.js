3f9caed15609bd38c41e1fce54b00592b40450aa
Pedrinnnnn <Pedrindbr2@gmail.com>
2023-02-26 11:43:23 -0300

    Principal

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const port = 3000
+const { resolve } = require('path');
+
+
+app.set('views', resolve('./views'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.render("home");
+
+})
+
+app.get('/login',(req,res)=>{
+    res.render("login");
+
+})
+
+
+app.listen(3000,function(){
+    console.log('Ok');
+})
+
