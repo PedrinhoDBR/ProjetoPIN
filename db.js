3bbed716aa7971a00d1b6b399b412ce2484cac9b
Maitê Emanuely <maite.emanuely.2013@gmail.com>
2023-11-03 17:09:59 -0300

    a maitê é incrível

--- db.js
+++ db.js
@@ -1,18 +1,11 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('projetopin','root','1234',
+const sequelize = new Sequelize('projetopin','root','root123',
     {
     host: 'localhost',
     dialect: 'mysql'
     }
 );
 
-// const sequelize = new Sequelize('bwwgtapwu7ld6b2pmfce','uyljbougwprsnhbn','zygT7eoW1GzWNuXyOJL4',
-//     {
-//     host: 'bwwgtapwu7ld6b2pmfce-mysql.services.clever-cloud.com',
-//     dialect: 'mysql',
-//     port:3306
-//     }
-// );
 
 module.exports = sequelize
